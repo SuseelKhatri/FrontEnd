e053754eef02c0d4a7f6a8d9c7082dd310608518
SuseelKhatri <sushil.khatri.np@gmail.com>
2024-11-26 00:38:31 +0545

    admin dashboard

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
-  styleUrl: './admin-layout.component.css'
+  styleUrls: ['./admin-layout.component.css']
 })
 export class AdminLayoutComponent {
+  toggleSidebar(): void {
 
+    console.log("Hello");
+    const sidebar = document.getElementById('sidebar');
+    const content = document.getElementById('content');
+    if (sidebar && content) {
+      sidebar.classList.toggle('active');
+      content.classList.toggle('active');
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-// for routing import
-import { routes } from './app/app-routing.module';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { AppComponent } from './app/app.component';
+// import { provideRouter } from '@angular/router';
+// // for routing import
+// import { routes } from './app/app-routing.module';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
-}).catch(err => console.error(err));
+// bootstrapApplication(AppComponent, {
+//   providers: [provideRouter(routes)],
+// }).catch(err => console.error(err));
+
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
