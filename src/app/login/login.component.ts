752d2333b2a81ef256b3ebca721944c3ea8a8f07
SuseelKhatri <sushil.khatri.np@gmail.com>
2024-11-26 22:29:19 +0545

    login changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  @HostListener('document:mousemove', ['$event'])
+    onMouseMove(event: MouseEvent) {
+        const background = document.getElementById('gradientBackground');
+        if (background) {
+            const xPercent = (event.clientX / window.innerWidth - 0.5) * 100;
+            const yPercent = (event.clientY / window.innerHeight - 0.5) * 100;
+            background.style.backgroundPosition = `${50 + xPercent}% ${50 + yPercent}%`;
+        }
+    }
 
 }
